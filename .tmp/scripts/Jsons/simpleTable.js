18507f6fefe7356ab1d4b2f5eb5f51dc3d7a6afd
egiacalone <egiacalone@deloitte.it>
2019-02-10 11:13:13 +0100

    first commit

--- .tmp/scripts/Jsons/simpleTable.js
+++
@@ -1,3 +0,0 @@
-// grid dummy data
-export var newSimpleTable = { "table": [{ "Id ": 1, "square ": 1 }, { "Id ": 2, "square ": 3 }, { "Id ": 3, "square ": 9 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }, { "id": 2, "square": 3 }] };
-//# sourceMappingURL=simpleTable.js.map
--- .tmp/scripts/partialJs/_nodeOperations.js
+++
@@ -1,8 +0,0 @@
-export function newNodeOperations(dom) {
-    // _NodeOperations.js
-
-    var DOMtree = { 'saluto': 'hello world!' };
-
-    return DOMtree.saluto;
-}
-//# sourceMappingURL=_nodeOperations.js.map
